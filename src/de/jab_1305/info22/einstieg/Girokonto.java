8438121bdf1f5948f35fddfc7785fd8e6eebdfdb
JAB1305 <54218874+JAB1305@users.noreply.github.com>
2022-05-24 15:17:04 +0200

    ÜÜÜberweisungen
    Co-Authored-By: AriBirgisson <100857672+AriBirgisson@users.noreply.github.com>

--- src/de/jab_1305/info22/einstieg/Girokonto.java
+++ src/de/jab_1305/info22/einstieg/Girokonto.java
@@ -1,5 +1,8 @@
 package de.jab_1305.info22.einstieg;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Girokonto {
     private float skipper;
     private String besitzer;
@@ -8,25 +11,53 @@ public class Girokonto {
     // SO i got this game where you tell my pronouns 5 times very fast, ok?
     // So you say nick, her very fast
     private boolean fubnktioniertnichtfragezeichren;
-    private  String nummer;
+    private String nummer;
+    private List<String> history;
 
     public Girokonto(String schorscho, float skipper, String neuenummer) {
         this.skipper = skipper;
         this.besitzer = schorscho;
         this.fubnktioniertnichtfragezeichren = false;
         this.nummer = neuenummer;
+        this.history = new ArrayList<>();
+    }
+
+    private boolean abziehen(float jf) {
+        if (fubnktioniertnichtfragezeichren) return false;
+        boolean legal = jf < skipper;
+        if (!legal) return false;
+        this.skipper -= jf;
+        return true;
     }
 
     public boolean eimzahlen(float zj) {
         this.skipper += zj;
+        history.add("Hallo du hascht eimbezahlt ganze " + zj);
         return true;
     }
 
-    public boolean geklapptfrageezeichen(float jf) {
-        if (fubnktioniertnichtfragezeichren) return false;
-        if (jf > skipper) return false;
+    public boolean üüüüüüberwiesung(Girokonto konto1, float money) {
+        if (this.kaleunFürAndersson(konto1, money)) { // Wenn Geld weg ist
+            return konto1.gebenVomAndersson(this, money); // Dem anderen geben
+        }
+        return false;
+    }
 
-        this.skipper -= jf;
+    public boolean abgezoben(float jf) {
+        boolean abziehen = abziehen(jf);
+        if (abziehen) history.add("Dein Money ist weg. jf: " + jf);
+        return abziehen;
+    }
+
+    private boolean kaleunFürAndersson(Girokonto konto1, float jf) {
+        boolean abziehen = abziehen(jf);
+        if (abziehen) history.add("Dein Money ist weg. Der da hats jetzt:" + konto1.getBesitzer() + " jf: " + jf);
+        return abziehen;
+    }
+
+    private boolean gebenVomAndersson(Girokonto konto1, float jf) {
+        this.skipper += jf;
+        history.add("Hallo du hascht vom " + konto1.getBesitzer() + " von Andersson ganze " + jf + " gegeben bekommen");
         return true;
     }
 
@@ -36,5 +67,21 @@ public class Girokonto {
 
     public String nenigalngelneuenummer() {
         return nummer;
+    }
+
+    public String getBesitzer() {
+        return besitzer;
+    }
+
+    public String generateKontoAuszug() {
+        StringBuilder stringBuilder = new StringBuilder("Kontoauszug:");
+        stringBuilder.append("\n");
+
+        for (String spinnenmann : this.history) {
+            stringBuilder.append(spinnenmann);
+            stringBuilder.append("\n");
+        }
+
+        return stringBuilder.toString();
     }
 }
--- src/de/jab_1305/info22/gsj.java
+++ src/de/jab_1305/info22/gsj.java
@@ -5,10 +5,23 @@ import de.jab_1305.info22.einstieg.Girokonto;
 public class gsj {
     public static void main(String[] args) {
         String baumOhneLisbelnICHSLIBELSNICHT = "Hallo"; // Ari: Ich kann das nicht lesen
-        Girokonto schorschosKontoMitNiceVielMoney = new Girokonto("Schorscho", 69.420f);
+        Girokonto schorschosKontoMitNiceVielMoney = new Girokonto("Schorscho",
+                69.420f,
+                "Ich hab ne neue Nummer ne neue Nummer");
+
+        Girokonto AndersVonAnderssonSeinErspartesFrankens = new Girokonto("Sndersson",
+                13.00f,
+                "Ne Niegel Nagel Neue Nummer");
+
         schorschosKontoMitNiceVielMoney.eimzahlen(3f);
+        schorschosKontoMitNiceVielMoney.üüüüüüberwiesung(AndersVonAnderssonSeinErspartesFrankens, 2.00f);
+
+        System.out.println(AndersVonAnderssonSeinErspartesFrankens.getSkipper());
 
         // PYTHON: print("Hallo) -> System.out.println("Hallo")
         System.out.println(schorschosKontoMitNiceVielMoney.getSkipper());
+
+        System.out.println("\n\n");
+        System.out.println(schorschosKontoMitNiceVielMoney.generateKontoAuszug());
     }
 }
